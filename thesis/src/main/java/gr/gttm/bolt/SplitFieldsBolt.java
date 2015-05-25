0072cf527f53d27954b224acad4d4bcf7ddb84a6
gttm <g.touloupas@gmail.com>
2015-05-26 02:05:09 +0300

    Moved ipToInt computation inside the topology

--- thesis/src/main/java/gr/gttm/bolt/SplitFieldsBolt.java
+++ thesis/src/main/java/gr/gttm/bolt/SplitFieldsBolt.java
@@ -15,19 +15,40 @@ public class SplitFieldsBolt extends BaseBasicBolt {
 		String line = tuple.getString(0);
 		String[] fields = line.split(" ");
 
-		if (fields.length == 9) {
+		if (fields.length == 7) {
+			String sourceIp = fields[0];
+			long sourceIpInt = ipToInt(sourceIp);
+			String destinationIp = fields[1];
+			long destinationIpInt = ipToInt(destinationIp);
+			String protocol = fields[2];
+			String sourcePort = fields[3];
+			String destinationPort = fields[4];
+			String ipSize = fields[5];
+			String date = fields[6];
+			
 			// default stream
-			collector.emit(new Values((Object[]) fields));
+			collector.emit(new Values(sourceIp, sourceIpInt,
+					destinationIp, destinationIpInt, protocol,
+					sourcePort, destinationPort, ipSize, date));
+			
 			// stream portStream
-			collector.emit("portStream", new Values(fields[6]));
-			collector.emit("portStream", new Values(fields[7]));
+			collector.emit("portStream", new Values(sourcePort));
+			collector.emit("portStream", new Values(destinationPort));
 		}
+	}
+
+	public long ipToInt(String ipString) {
+		String[] ipOctets = ipString.split("\\.");
+		return Long.parseLong(ipOctets[0]) * 16777216
+				+ Long.parseLong(ipOctets[1]) * 65536
+				+ Long.parseLong(ipOctets[2]) * 256
+				+ Long.parseLong(ipOctets[3]);
 	}
 
 	@Override
 	public void declareOutputFields(OutputFieldsDeclarer declarer) {
-		declarer.declare(new Fields("sourceIp", "sourceIpInt",
-				"destinationIp", "destinationIpInt", "protocol", "sourcePort",
+		declarer.declare(new Fields("sourceIp", "sourceIpInt", "destinationIp",
+				"destinationIpInt", "protocol", "sourcePort",
 				"destinationPort", "ipSize", "date"));
 		declarer.declareStream("portStream", new Fields("port"));
 	}
