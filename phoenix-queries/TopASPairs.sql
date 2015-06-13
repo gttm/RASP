8099d474e2591ddf00c072911c88cbf4fe823c4d
gttm <g.touloupas@gmail.com>
2015-06-13 21:55:54 +0300

    Add TopPairQueryPrepare.py

--- phoenix-queries/TopASPairs.sql
+++ phoenix-queries/TopASPairs.sql
@@ -1,6 +1,6 @@
-SELECT "as"."asS", "as"."asD", COUNT(1) AS "pairCount" 
-FROM "netdata" 
-WHERE "time" > 1434017860
-GROUP BY "as"."asS", "as"."asD" 
-ORDER BY "pairCount" DESC 
+SELECT "as"."asS", "as"."asD", COUNT(1) AS "pairCount"
+FROM "netdata"
+WHERE "time" > 1434144924000000 and "time" < 1434146124000000
+GROUP BY "as"."asS", "as"."asD"
+ORDER BY "pairCount" DESC
 LIMIT 10;
--- phoenix-queries/TopDNSPairs.sql
+++ phoenix-queries/TopDNSPairs.sql
@@ -1,6 +1,6 @@
-SELECT "dns"."dnsS", "dns"."dnsD", COUNT(1) AS "pairCount" 
-FROM "netdata" 
-WHERE "time" > 1434017860
-GROUP BY "dns"."dnsS", "dns"."dnsD" 
-ORDER BY "pairCount" DESC 
+SELECT "dns"."dnsS", "dns"."dnsD", COUNT(1) AS "pairCount"
+FROM "netdata"
+WHERE "time" > 1434144920000000 and "time" < 1434146120000000
+GROUP BY "dns"."dnsS", "dns"."dnsD"
+ORDER BY "pairCount" DESC
 LIMIT 10;
